43f437b8cb888fe1011de596d4ec1684c8a9b9e9
fitronansori <fitronansori7@gmail.com>
2024-05-23 20:18:04 +0800

    chore: Add root component

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layouts/Header";
+import { SignOut } from "@/components/SignOut";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <SignOut />
+    </>
+  );
+};
+export default Home;
